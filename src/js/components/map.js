11529c7355022e190bf9c9f5dab4e773d4bdd3f3
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2024-03-29 11:30:04 +0300

    Добавляет карту

---
+++ src/js/components/map.js
@@ -0,0 +1,23 @@
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [56.00453829737664,37.864109246032726],
+    zoom: 17
+  });
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+// let placemark = new ymaps.placemark([], {}, {
+
+// });
+
+
+
+ymaps.ready(init);
